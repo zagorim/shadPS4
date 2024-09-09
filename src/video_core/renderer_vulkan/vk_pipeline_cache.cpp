427882d28d070a404173dcddd1f88447c5519c54
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-09-09 11:16:09 -0600

    Regression fix

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -174,9 +174,7 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 24> skip_hashes = {
-        0x2da7fe60, 0x8e3f8dc4, 0x42f2a521,
-        0xa509af23, 0x4ca76892, 0xa954e79d,
+    static constexpr std::array<u64, 18> skip_hashes = {
         0x1635154c, 0x77d1c63,  0xff7a6d7c, 
         0x4899010a, 0xc8854a11, 0xaa9d023d,
         0x17a64a21, 0x94ec4dfb, 0xbddb8fc7,
