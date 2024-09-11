2dad1bbb15c9aa392f3052573374cf5f02905ece
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-09-11 17:29:50 -0600

    Linux build

--- src/core/libraries/kernel/thread_management.cpp
+++ src/core/libraries/kernel/thread_management.cpp
@@ -1062,6 +1062,7 @@ int PS4_SYSV_ABI scePthreadCreate(ScePthread* thread, const ScePthreadAttr* attr
 ScePthread PThreadPool::Create(const char* name) {
     std::scoped_lock lock{m_mutex};
 
+
     for (auto* p : m_threads) {
         if (p->is_free && p->name == name) {
             p->is_free = false;
@@ -1069,7 +1070,16 @@ ScePthread PThreadPool::Create(const char* name) {
         }
     }
 
+#ifdef _WIN64
     auto* ret = new PthreadInternal{};
+#else
+    // TODO: Linux specific hack
+    static u8* hint_address = reinterpret_cast<u8*>(0x7FFFFC000ULL);
+    auto* ret = reinterpret_cast<PthreadInternal*>(
+        mmap(hint_address, sizeof(PthreadInternal), PROT_READ | PROT_WRITE,
+             MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0));
+    hint_address += Common::AlignUp(sizeof(PthreadInternal), 4_KB);
+#endif
     ret->is_free = false;
     ret->is_detached = false;
     ret->is_almost_done = false;
