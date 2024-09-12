a60d6c4a30a0f15bea937c574d925653690ea7a9
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-09-12 05:51:45 -0600

    Just Blood Skips

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -188,7 +188,8 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 0> skip_hashes = {};
+    static constexpr std::array<u64, 4> skip_hashes = {0xaa9d023d, 0x17a64a21, 0x94ec4dfb, 0xbddb8fc7};
+
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
