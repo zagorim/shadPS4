14fe7cfce75544667c55cca037c2ccffdae1b3af
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-09-12 02:19:52 -0600

    Linux Fix v2

--- src/core/libraries/kernel/thread_management.cpp
+++ src/core/libraries/kernel/thread_management.cpp
@@ -1059,15 +1059,8 @@ int PS4_SYSV_ABI scePthreadCreate(ScePthread* thread, const ScePthreadAttr* attr
     }
 }
 
-ScePthread PThreadPool::Create(const char* name) {
-    std::scoped_lock lock{m_mutex};
-
-
-    for (auto* p : m_threads) {
-        if (p->is_free && p->name == name) {
-            p->is_free = false;
-            return p;
-        }
+@@ -1065,7 +1065,16 @@ScePthread PThreadPool::Create() {
+         }
     }
 
 #ifdef _WIN64
