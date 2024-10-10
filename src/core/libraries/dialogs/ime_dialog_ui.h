a5845fa341eb264cf34f9314d4cc185c86754dd6
Dmugetsu <168934208+diegolix29@users.noreply.github.com>
2024-10-10 11:44:42 -0600

    Update ime_dialog_ui.h

--- src/core/libraries/dialogs/ime_dialog_ui.h
+++ src/core/libraries/dialogs/ime_dialog_ui.h
@@ -4,12 +4,10 @@
 #pragma once
 
 #include <mutex>
-#include <vector>
 #ifndef _WIN32
 #include <iconv.h>
 #endif
 #include <imgui.h>
-#include "common/cstring.h"
 #include "common/types.h"
 #include "core/libraries/dialogs/ime_dialog.h"
 #include "imgui/imgui_layer.h"
@@ -32,11 +30,11 @@ class ImeDialogState final {
     OrbisImeExtKeyboardFilter keyboard_filter{};
     u32 max_text_length{};
     char16_t* text_buffer{};
-    std::vector<char> title;
-    std::vector<char> placeholder;
+    char* title = nullptr;
+    char* placeholder = nullptr;
 
     // A character can hold up to 4 bytes in UTF-8
-    Common::CString<ORBIS_IME_DIALOG_MAX_TEXT_LENGTH * 4> current_text;
+    char current_text[ORBIS_IME_DIALOG_MAX_TEXT_LENGTH * 4 + 1] = {0};
 #ifndef _WIN32
     iconv_t orbis_to_utf8 = (iconv_t)-1;
     iconv_t utf8_to_orbis = (iconv_t)-1;
