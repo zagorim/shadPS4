d2d47bc1794f49e92670bd0726a198438b20161c
Dmugetsu <168934208+diegolix29@users.noreply.github.com>
2024-10-13 06:07:59 -0600

    Update sdl_audio.h

--- src/audio_core/sdl_audio.h
+++ src/audio_core/sdl_audio.h
@@ -3,104 +3,37 @@
 
 #pragma once
 
-#include <string>
-#include "common/types.h"
+#include <shared_mutex>
+#include <SDL3/SDL_audio.h>
+#include "core/libraries/audio/audioout.h"
 
-#include <SDL3/SDL_events.h>
+namespace Audio {
 
-struct SDL_Window;
-struct SDL_Gamepad;
-union SDL_Event;
-
-namespace Input {
-class GameController;
-}
-
-namespace Frontend {
-
-class KeyBinding {
+class SDLAudio {
 public:
-    Uint32 key;
-    SDL_Keymod modifier;
-    KeyBinding(SDL_Keycode k, SDL_Keymod m) : key(k), modifier(m){};
-    bool operator<(const KeyBinding& other) const;
-    ~KeyBinding(){};
-};
+    SDLAudio() = default;
+    virtual ~SDLAudio() = default;
 
-enum class WindowSystemType : u8 {
-    Headless,
-    Windows,
-    X11,
-    Wayland,
-    Metal,
-};
-
-struct WindowSystemInfo {
-    // Connection to a display server. This is used on X11 and Wayland platforms.
-    void* display_connection = nullptr;
-
-    // Render surface. This is a pointer to the native window handle, which depends
-    // on the platform. e.g. HWND for Windows, Window for X11. If the surface is
-    // set to nullptr, the video backend will run in headless mode.
-    void* render_surface = nullptr;
-
-    // Scale of the render surface. For hidpi systems, this will be >1.
-    float render_surface_scale = 1.0f;
-
-    // Window system type. Determines which GL context or Vulkan WSI is used.
-    WindowSystemType type = WindowSystemType::Headless;
-};
-
-class WindowSDL {
-public:
-    explicit WindowSDL(s32 width, s32 height, Input::GameController* controller,
-                       std::string_view window_title);
-    ~WindowSDL();
-
-    s32 getWidth() const {
-        return width;
-    }
-
-    s32 getHeight() const {
-        return height;
-    }
-
-    bool isOpen() const {
-        return is_open;
-    }
-
-    [[nodiscard]] SDL_Window* GetSdlWindow() const {
-        return window;
-    }
-
-    WindowSystemInfo getWindowInfo() const {
-        return window_info;
-    }
-
-    void waitEvent();
-    void updateMouse();
+    s32 AudioOutOpen(int type, u32 samples_num, u32 freq,
+                     Libraries::AudioOut::OrbisAudioOutParamFormat format);
+    s32 AudioOutOutput(s32 handle, const void* ptr);
+    s32 AudioOutSetVolume(s32 handle, s32 bitflag, s32* volume);
+    s32 AudioOutGetStatus(s32 handle, int* type, int* channels_num);
 
 private:
-    void onResize();
-    void onKeyPress(const SDL_Event* event);
-    void onGamepadEvent(const SDL_Event* event);
-    int sdlGamepadToOrbisButton(u8 button);
-
-    void updateModKeyedInputsManually(KeyBinding& binding);
-    void updateButton(KeyBinding& binding, u32 button, bool isPressed);
-    static Uint32 keyRepeatCallback(void* param, Uint32 id, Uint32 interval);
-    static Uint32 mousePolling(void* param, Uint32 id, Uint32 interval);
-
-    void parseInputConfig(const std::string& filename);
-
-private:
-    s32 width;
-    s32 height;
-    Input::GameController* controller;
-    WindowSystemInfo window_info{};
-    SDL_Window* window{};
-    bool is_shown{};
-    bool is_open{true};
+    struct PortOut {
+        SDL_AudioStream* stream = nullptr;
+        u32 samples_num = 0;
+        u32 freq = 0;
+        u32 format = -1;
+        int type = 0;
+        int channels_num = 0;
+        int volume[8] = {};
+        u8 sample_size = 0;
+        bool isOpen = false;
+    };
+    std::shared_mutex m_mutex;
+    std::array<PortOut, Libraries::AudioOut::SCE_AUDIO_OUT_NUM_PORTS> portsOut;
 };
 
-} // namespace Frontend
+} // namespace Audio
