c6682a7edd098b3b99feda141db347792a9365e7
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-10-30 23:51:57 -0600

    CC

--- src/qt_gui/kbm_help_dialog.cpp
+++ src/qt_gui/kbm_help_dialog.cpp
@@ -4,70 +4,70 @@
 #include "kbm_help_dialog.h"
 
 #include <QApplication>
-#include <QLabel>
-#include <QWidget>
+
 #include <QGroupBox>
-#include <QVBoxLayout>
+
 #include <QHBoxLayout>
+#include <QLabel>
 #include <QPushButton>
 #include <QScrollArea>
+#include <QVBoxLayout>
+#include <QWidget>
 
 class ExpandableSection : public QWidget {
 public:
-    ExpandableSection(const QString &title, const QString &content, QWidget *parent = nullptr)
+    ExpandableSection(const QString& title, const QString& content, QWidget* parent = nullptr)
         : QWidget(parent) {
-        QVBoxLayout *layout = new QVBoxLayout;
+        QVBoxLayout* layout = new QVBoxLayout;
 
         // Create a button for the section title
-        QPushButton *toggleButton = new QPushButton(title);
+        QPushButton* toggleButton = new QPushButton(title);
         layout->addWidget(toggleButton);
 
         // Create a label for the content (initially hidden) with word wrap
-        QLabel *contentLabel = new QLabel(content);
+        QLabel* contentLabel = new QLabel(content);
         contentLabel->setWordWrap(true);
         contentLabel->setVisible(false); // Hidden by default
         layout->addWidget(contentLabel);
 
         // Connect button click to toggle content visibility
-        connect(toggleButton, &QPushButton::clicked, [contentLabel]() {
-            contentLabel->setVisible(!contentLabel->isVisible());
-        });
+        connect(toggleButton, &QPushButton::clicked,
+                [contentLabel]() { contentLabel->setVisible(!contentLabel->isVisible()); });
 
         // Set spacing to 0 for minimal spacing
-        layout->setSpacing(2); // Minimal spacing between sections
+        layout->setSpacing(2);                  // Minimal spacing between sections
         layout->setContentsMargins(0, 0, 0, 0); // No margins for top, left, right, bottom
 
         setLayout(layout);
     }
 };
 
-HelpDialog::HelpDialog(QWidget *parent) : QDialog(parent) {
+HelpDialog::HelpDialog(QWidget* parent) : QDialog(parent) {
     // Main layout for the help dialog
-    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QVBoxLayout* mainLayout = new QVBoxLayout;
 
     // Add expandable sections
-        mainLayout->addWidget(new ExpandableSection("Quickstart", quickstart()));
-        mainLayout->addWidget(new ExpandableSection("FAQ", faq()));
-        mainLayout->addWidget(new ExpandableSection("Syntax", syntax()));
-        mainLayout->addWidget(new ExpandableSection("Special Bindings", special()));
-        mainLayout->addWidget(new ExpandableSection("Keybindings", bindings()));
+    mainLayout->addWidget(new ExpandableSection("Quickstart", quickstart()));
+    mainLayout->addWidget(new ExpandableSection("FAQ", faq()));
+    mainLayout->addWidget(new ExpandableSection("Syntax", syntax()));
+    mainLayout->addWidget(new ExpandableSection("Special Bindings", special()));
+    mainLayout->addWidget(new ExpandableSection("Keybindings", bindings()));
 
     // Create a widget to hold all sections (with unified scrollable area)
-    QWidget *containerWidget = new QWidget;
+    QWidget* containerWidget = new QWidget;
     containerWidget->setLayout(mainLayout);
 
     // Create a scroll area to wrap all content
-    QScrollArea *scrollArea = new QScrollArea;
+    QScrollArea* scrollArea = new QScrollArea;
     scrollArea->setWidgetResizable(true);
     scrollArea->setWidget(containerWidget);
 
     mainLayout->setAlignment(Qt::AlignTop);
 
     // Main layout for the Help dialog
-    QVBoxLayout *dialogLayout = new QVBoxLayout;
+    QVBoxLayout* dialogLayout = new QVBoxLayout;
     dialogLayout->addWidget(scrollArea);
     setLayout(dialogLayout);
 
     setMinimumSize(500, 400); // Set a reasonable window size// Set the layout for the dialog
-
 }
